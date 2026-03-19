9bebd6dfac8102e8188f832d8be6e10f0a15d1ee
SegfaultCreator <53517431+SegfaultCreator@users.noreply.github.com>
2026-03-19 18:51:44 +0100

    Refactor: Restructured Main to add more potential calculation cases

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from DataOutput.GeoJsonCreator import GeoJsonCreator
 from DataOutput.PrintOutput import PrintOutput
 from DataOutput.StreetPlot import StreetPlot
 
-area_under_creation = "München"
+area_under_creation = "isarvorstadt"
 
 
 if(area_under_creation != "München"):
@@ -26,11 +26,9 @@ else:
 
 
 #######################################
-# Phase 1: Retrieve data
+# Retrieve (and print) raw data
 #######################################
 
-########
-# Straßen
 sr = SegmentRetrieval()
 streets_gdf = sr.fetch_as_geodataframe(used_bbox)
 
@@ -48,28 +46,33 @@ if educational_bdg_gdf is not None:
     print(f"Total schools found: {len(educational_bdg_gdf)}")
 
 #######################################
-# Phase 2: Check for Potential
+# Identify potential
 #######################################
 
 # Zebra crossings
 print("Identifying Zebra Crossings ...")
 zebra_potential_result : PotentialCalculationResult = ZebraPotential.find_tempo50_segments_near_zebra(streets_gdf = streets_gdf, zebras_gdf = zebra_gdf, search_distance_m = 15)
-streets_updated_gdf = SpeedAnnotationUpdater.update_speed_annotation(streets_gdf = streets_gdf, osm_ids_to_annotate = zebra_potential_result.street_ids, new_val = "T30_Potenzial_Zebrastreifen")
-zebra_gdf["potential_candidate"] = zebra_gdf["osm_id"].isin(zebra_potential_result.opt_source_ids)
-
-# Educational buildings
 print("Identifiying Street near schools")
 educational_potential_result : PotentialCalculationResult = ProximityPotential.find_tempo50_segments_near_features(streets_gdf = streets_gdf, features_gdf = educational_bdg_gdf, search_distance_m = 50)
-streets_updated_gdf = SpeedAnnotationUpdater.update_speed_annotation(streets_gdf = streets_updated_gdf, osm_ids_to_annotate = educational_potential_result.street_ids, new_val = "T30_Potenzial_Schule")
-educational_bdg_gdf["potential_candidate"] = educational_bdg_gdf["osm_id"].isin(educational_potential_result.opt_source_ids)
-
-# Lückenschluss
 print("Identifying Gaps ...")
 gap_potential_result : PotentialCalculationResult= Tempo50GapPotential.find_all_tempo_50_gaps(gdf = streets_gdf)
+
+#######################################
+# Create street dataset w. Annotations
+#######################################
+streets_updated_gdf = SpeedAnnotationUpdater.update_speed_annotation(streets_gdf = streets_gdf, osm_ids_to_annotate = zebra_potential_result.street_ids, new_val = "T30_Potenzial_Zebrastreifen")
+streets_updated_gdf = SpeedAnnotationUpdater.update_speed_annotation(streets_gdf = streets_updated_gdf, osm_ids_to_annotate = educational_potential_result.street_ids, new_val = "T30_Potenzial_Schule")
 streets_updated_gdf = SpeedAnnotationUpdater.update_speed_annotation(streets_gdf = streets_updated_gdf, osm_ids_to_annotate = gap_potential_result.street_ids, new_val = "T30_Potenzial_Luecke")
 
 #######################################
-# Phase 3: File / Print / Map Output
+# Annotate sources
+#######################################
+
+zebra_gdf["potential_candidate"] = zebra_gdf["osm_id"].isin(zebra_potential_result.opt_source_ids)
+educational_bdg_gdf["potential_candidate"] = educational_bdg_gdf["osm_id"].isin(educational_potential_result.opt_source_ids)
+
+#######################################
+# File / Print / Map Output
 #######################################
 
 streets_with_potential = streets_updated_gdf.loc[streets_updated_gdf["maxspeed_class"].isin(["T30_Potenzial_Zebrastreifen", "T30_Potenzial_Luecke", "T30_Potenzial_Schule"])]
